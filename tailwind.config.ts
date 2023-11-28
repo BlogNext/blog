6cb458ac3d71bd7f7a439ef1b6f9bee184a0feaf
LaughingZhu <474268433@qq.com>
2023-11-29 01:50:37 +0800

    feat(*): custom animation tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      animation: {
+        toBottom: 'toBottom .4s cubic-bezier(0, 0, 0.07, 1)'
+      },
+      keyframes: {
+        toBottom: {
+          '0%': { scale: '1', transform: 'rotate(0deg)' },
+          '100%': { scale: '1.2', transform: 'rotate(360deg)' }
+        }
+      }
+    }
   },
-  plugins: [],
-}
-export default config
+  plugins: []
+};
+export default config;
