2aaab9321414a2cb677495ac65b34a0deb136ec5
LaughingZhu <474268433@qq.com>
2023-11-30 01:39:09 +0800

    feat(*): main header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,19 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className='flex-auto flex py-8 flex-col items-center justify-between'>
-      <div className='flex w-full flex-auto flex-col justify-start align-top border-y-rose-700'>
-        111
+    <main className='flex flex-auto flex-col items-center justify-between'>
+      <div className='flex w-full flex-auto flex-col justify-start border-y-rose-700 align-top'>
+        <div className='relative flex w-full flex-col items-center justify-center bg-[#1D1F20] p-[20px] text-[30px] leading-normal text-[#aaa]'>
+          <div
+            className='slogan relative'
+            data-spotlight="LaughingZhu's Blog"
+          >{`LaughingZhu's Blog`}</div>
+          <div className='mt-[18px] text-[18px] tracking-[3px]'>
+            桌上的文字太拥挤 想出去走走去散心
+          </div>
+        </div>
       </div>
-      <div className='text-center w-full'>
+      <div className='w-full text-center'>
         <span>Copyright © 2020-2021 LaughingZhu 版权所有 </span>
         <a href='https://beian.miit.gov.cn/' target='_blank'>
           京ICP备2020039821号-1
