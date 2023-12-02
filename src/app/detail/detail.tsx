f17cef594d303f6a4692e709179023a107e1bc92
LaughingZhu <474268433@qq.com>
2023-12-02 16:05:01 +0800

    feat(*): 优化

--- src/app/detail/detail.tsx
+++ src/app/detail/detail.tsx
@@ -3,13 +3,14 @@ import { getDetail } from '@/server/api/query';
 import { useQuery } from '@tanstack/react-query';
 
 export default function Detail() {
-  const { data } = useQuery({ queryKey: ['posts'], queryFn: getDetail });
+  const { data, isFetching, isError } = useQuery({ queryKey: ['posts'], queryFn: getDetail });
   // This query was not prefetched on the server and will not start
   // fetching until on the client, both patterns are fine to mix.
   // const { data: commentsData } = useQuery({
   //   queryKey: ['posts-comments'],
   //   queryFn: getComments,
   // })
-  console.log(data, Math.random());
+  if (isFetching) return <div className='flex-auto'>loading...</div>;
+  if (isError) return <div className='flex-auto'>error...</div>;
   return <div className='flex-auto'>这是服务端渲染吗{data?.data}</div>;
 }
