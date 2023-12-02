56ea36adc6e8d4cf9c5490f4fdd6a8c6194dd128
LaughingZhu <474268433@qq.com>
2023-12-02 15:37:30 +0800

    feat(*): config/

---
+++ src/config/config.prod.ts
@@ -0,0 +1,11 @@
+export default {
+  local_key: {
+    // 用户token
+    user_auth: 'sudoku_id'
+  },
+  base_url: 'https://sudoku.cn',
+  // 七牛访问
+  qiniu_base: 'https://cdn.sudokucup.cn/',
+  // 七牛上传
+  qiniu_upload: 'https://upload-z1.qiniup.com'
+};
