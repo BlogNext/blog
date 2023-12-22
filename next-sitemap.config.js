bac8f022ee927e97046d043f4326f6fbe90a688c
LaughingZhu <474268433@qq.com>
2023-12-23 00:32:10 +0800

    feat(*): sitemap noindex

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://blog.laughingzhu.cn',
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  generateIndexSitemap: false
 };
